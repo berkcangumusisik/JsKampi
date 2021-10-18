28c81cf468f156a12df8dd647e1144c82315f070
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-10-18 23:09:11 +0300

    Ders 1 Part 2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 console.log("Merhaba Kodlama.io");
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+const euroDun = 11.2
+console.log(euroDun)
