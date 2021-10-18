e0f7f981be40858f062d1dafb6f95120852bec91
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-10-19 00:06:16 +0300

    Ders 1 Son part

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ dolarDun = "9.20"
 }
 console.log(dolarDun)
 const euroDun = 11.2
+//euroDun = 11 //Bunu yapamıyoruz
 console.log(euroDun)
+
+//array
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+   console.log("<li>" + konutKredileri[i] + "</li>")
+}
+
+console.log("</ul>")
